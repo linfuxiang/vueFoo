fc7d23f985bb4820691bce50520f0bd8005719be
linfuxiang <linfuxiang@yaochufa.com>
2017-04-27 17:28:35 +0800

    modified

--- src/stores/modules/charts.js
+++ src/stores/modules/charts.js
@@ -8,51 +8,55 @@ export default {
     state: {
         header: '实时空气质量指数(AQI)监测',
         jsonData: [],
-        showWhichComponent: ''
+        showWhichComponent: '',
+        chartsArr: [
+            'lines',
+            'bar',
+            'geo',
+        ],
+        i: 0,
     },
     mutations: {
         charts_initComponent(state) {
-            state.showWhichComponent = 'geo';
+            state.showWhichComponent = state.chartsArr[state.i];
         },
-    	charts_change(state) {
-            if(state.showWhichComponent == 'geo'){
-                state.showWhichComponent = 'bar';
-            } else if(state.showWhichComponent == 'bar'){
-                state.showWhichComponent = 'lines';
-            } else if(state.showWhichComponent == 'lines') {
-                state.showWhichComponent = 'geo';
+        charts_change(state) {
+            state.i += 1;
+            if(state.i >= state.chartsArr.length) {
+                state.i = 0
             }
+            state.showWhichComponent = state.chartsArr[state.i];
         },
         charts_changeData(state, args) {
-        	state.jsonData = args.data;
+            state.jsonData = args.data;
         }
     },
     actions: {
-    	charts_getData({commit, state}) {
-            if(state.jsonData.length > 0){
+        charts_getData({ commit, state }) {
+            if (state.jsonData.length > 0) {
                 commit('global_hideLoading');
                 commit('charts_initComponent');
-                return ;
+                return;
             }
             // Vue.http.jsonp(GLOBAL_PATH.JSONP_URI + 'getData', {
-			Vue.http.get(GLOBAL_PATH.JSONP_URI + 'getData', {
-			    params: {
-			        'reqCollection': 'latest',
-			        'reqArea': ''
-			    }
-			}).then((res) => {
+            Vue.http.get(GLOBAL_PATH.JSONP_URI + 'getData', {
+                params: {
+                    'reqCollection': 'latest',
+                    'reqArea': ''
+                }
+            }).then((res) => {
                 let data = res.data;
                 // let data = JSON.parse(res.data);
-			    commit({
-			    	type: 'charts_changeData',
-			    	data: data.data
-			    });
-			    commit('global_hideLoading');
+                commit({
+                    type: 'charts_changeData',
+                    data: data.data
+                });
+                commit('global_hideLoading');
                 commit('charts_initComponent');
-			}, (err) => {
-			    console.log(err);
-			    commit('global_hideLoading');
-			});
-    	}    	
+            }, (err) => {
+                console.log(err);
+                commit('global_hideLoading');
+            });
+        }
     }
 }
